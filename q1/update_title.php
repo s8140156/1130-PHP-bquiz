6629949f3c7b805f64e8cbcea02961f689b7a05c
s8140156 <s8140156@gmail>
2023-12-01 09:27:13 +0800

    建立問卷調查前端列表

--- q1/update_title.php
+++ q1/update_title.php
@@ -7,6 +7,7 @@ if(isset($_POST['id'])){
 
 	if(!empty($_FILES['img']['tmp_name'])){
 		move_uploaded_file($_FILES['img']['tmp_name'],'./img/' .$_FILES['img']['name']);
+		// 這邊要注意使用move_uploaded_file當檔案被更改時, 檔名相同會用最新的去覆蓋前面的 **由於這邊的檔案命名是以"檔案名"為主 重複的檔案要小心
 		$row['img']=$_FILES['img']['name'];
 		$Title->save($row);
 	}
